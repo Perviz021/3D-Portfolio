4d2ec64d7c259fbe5ed90bd59b936dc9146b9697
Parviz Namazli <perviznamazli021@gmail.com>
2024-01-17 22:31:55 +0400

    fix: Computer Model and Loader

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+// import { HemisphereLight, PointLight } from "three";
+
+import CanvasLoader from "../Loader";
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+  // importing gltf model
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
-export default Computers
+  // in 3js write mesh instead of div
+  return (
+    <mesh>
+      {/* Adding lightness */}
+      <hemisphereLight intensity={1} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={0.75} //make it smaller
+        position={[0, -3.25, -1.5]} //changing x y z positions
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
+
+const ComputerCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      {/* Canvas loader work while model loading */}
+
+      <Suspense fallback={<CanvasLoader />}>
+        {/* Enabling to move the model left and right*/}
+        <OrbitControls
+          // zoom is not allowed
+          enableZoom={false}
+          // moving model in a specific angle
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputerCanvas;
