eb7370786d5af3b07e1fcb405931fc3f679d0406
Parviz Namazli <perviznamazli021@gmail.com>
2024-01-21 16:30:15 +0400

    fix: Hero Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,8 +18,9 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915eff]">Parviz</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D-visuals, user <br className="sm:block hidden" />{" "}
-            interfaces and web applications
+            Passionate Front-End Developer dedicated{" "}
+            <br className="sm:block hidden" /> to crafting exceptional web
+            experiences
           </p>
         </div>
       </div>
