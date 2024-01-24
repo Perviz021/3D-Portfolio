1253b8d7c9fca15df6424487069d86919bad4115
Parviz Namazli <perviznamazli021@gmail.com>
2024-01-24 13:21:39 +0400

    feat: Experience Component

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,8 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import cbar from "./company/cbar.png";
+import khazar from "./company/khazar.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +50,8 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  cbar,
+  khazar,
   carrent,
   jobit,
   tripguide,
